578b1cfd6d3686d8dca3575973e7d941100da29a
muzzayan-omaar <muzzayanomaar@gmail.com>
2025-10-11 02:36:00 +0400

    newsletter10000

--- routes/newsletter.js
+++ routes/newsletter.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const { subscribeNewsletter } = require('../controllers/newsletterController');
-
 const { getAllSubscribers, deleteSubscriber, subscribeNewsletter } = require('../controllers/newsletterController');
 
+// POST → Subscribe
 router.post('/', subscribeNewsletter);
+
+// GET → Fetch all
 router.get('/', getAllSubscribers);
+
+// DELETE → Remove a subscriber
 router.delete('/:id', deleteSubscriber);
 
 module.exports = router;
